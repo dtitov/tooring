11a48b855abfc78587ad11b626204a84986a19ca
Dmytro Titov <dmytro.titov@gmail.com>
2016-04-24 16:38:37 +0300

    Remove some overengineering settings.

--- src/main/java/com/uwc/tooring/TooringApplication.java
+++ src/main/java/com/uwc/tooring/TooringApplication.java
@@ -4,10 +4,8 @@ import com.hazelcast.config.Config;
 import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.spring.context.SpringManagedContext;
 import org.apache.commons.cli.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -103,31 +101,13 @@ public class TooringApplication implements CommandLineRunner {
     }
 
     @Bean
-    public SpringManagedContext managedContext() {
-        return new SpringManagedContext();
-    }
-
-    @Bean
-    public HazelcastInstance hazelcastInstance(@Value("${hazelcast.discovery:LAN}") String hazelcastDiscovery,
-                                               @Value("${aws.access.key:}") String accessKey,
-                                               @Value("${aws.secret.key:}") String secretKey) {
+    public HazelcastInstance hazelcastInstance() {
         Config config = new Config();
         config.setProperty(HAZELCAST_LOGGING_TYPE, NONE);
-        config.setManagedContext(managedContext());
 
         NetworkConfig networkConfig = config.getNetworkConfig();
-        switch (hazelcastDiscovery.toUpperCase()) {
-            case "AWS":
-                networkConfig.getJoin().getAwsConfig().setAccessKey(accessKey).setSecretKey(secretKey).setEnabled(true);
-                networkConfig.getJoin().getMulticastConfig().setEnabled(false);
-                break;
-            case "LAN":
-                networkConfig.setPort(5701);
-                networkConfig.setPortAutoIncrement(true);
-                break;
-            default:
-                throw new RuntimeException("Property \"hazelcast.discovery\" is wrong (available values are LAN and AWS");
-        }
+        networkConfig.setPort(5701);
+        networkConfig.setPortAutoIncrement(true);
 
         return Hazelcast.newHazelcastInstance(config);
     }
